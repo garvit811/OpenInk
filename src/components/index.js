aacac2c51ef3f65164da8023af33b4265ac37794
garvit811 <sonigarvit811@gmail.com>
2025-09-20 21:25:07 +0530

    refactor: organize component imports in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,17 @@
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import Container from "./container/Container";
-import Logo from "./logo";
-import Logout_btn from "./logout_btn";
-import Button from "./Button";
-import Input from "./input";
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Container from "./container/Container.jsx";
+import Logo from "./logo.jsx";
+import Logout_btn from "./logout_btn.jsx";
+import Button from "./Button.jsx";
+import Input from "./input.jsx";
 import Select from "./select";
-import PostCard from "./PostCard";
-import RTE from './RTE';
-import Protected from './AuthLayout'
-import Login  from './login';
-import Signup from './signup';
-import PostForm from "./post-form/postform";
+import PostCard from "./PostCard.jsx";
+import RTE from './RTE.jsx';
+import Protected from './AuthLayout.jsx'
+import Login  from './login.jsx';
+import Signup from './signup.jsx';
+import PostForm from "./post-form/PostForm.jsx";
 
 
 export { Header, Footer, Container, Logo, Logout_btn, Button, Input, Select, PostCard, RTE, Protected, Login, Signup, PostForm };
